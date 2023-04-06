37872568f7a17f90da212b058e5c12ecad072a5e
adityar8 <aditya.r8@gmail.com>
2023-04-06 18:24:37 +0530

    addng client

---
+++ client/src/App.js
@@ -0,0 +1,106 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          My Temple Blog
+        </a>
+        <nav>
+          <a href="" className="logo">
+            Login
+          </a>
+          <a href="" className="logo">
+            Register
+          </a>
+        </nav>
+      </header>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.pexels.com/photos/1089302/pexels-photo-1089302.jpeg?auto=compress&cs=tinysrgb&w=400&h=150&dpr=2"
+            alt=""
+          ></img>
+        </div>
+
+        <div className="content">
+          <h2>Temple from AngKor</h2>
+          <p className="info">
+            <a className="author">Aditya R</a>
+            <time>2022-04-01 15:45</time>
+          </p>
+          <p className="summary">
+            The Ta Prohm Temple (and its famous Angkor Wat tree) is one of the
+            most photogenic temples in Cambodia. It's known as the “Tomb Raider
+            temple”. Chances are you've seen pictures of this place before
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.pexels.com/photos/1089302/pexels-photo-1089302.jpeg?auto=compress&cs=tinysrgb&w=400&h=150&dpr=2"
+            alt=""
+          ></img>
+        </div>
+        <div className="content">
+          <h2>Temple from AngKor</h2>
+          <p className="info">
+            <a className="author">Aditya R</a>
+            <time>2022-04-01 15:45</time>
+          </p>
+          <p className="summary">
+            The Ta Prohm Temple (and its famous Angkor Wat tree) is one of the
+            most photogenic temples in Cambodia. It's known as the “Tomb Raider
+            temple”. Chances are you've seen pictures of this place before
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.pexels.com/photos/1089302/pexels-photo-1089302.jpeg?auto=compress&cs=tinysrgb&w=400&h=150&dpr=2"
+            alt=""
+          ></img>
+        </div>
+        <div className="content">
+          <h2>Temple from AngKor</h2>
+          <p className="info">
+            <a className="author">Aditya R</a>
+            <time>2022-04-01 15:45</time>
+          </p>
+          <p className="summary">
+            The Ta Prohm Temple (and its famous Angkor Wat tree) is one of the
+            most photogenic temples in Cambodia. It's known as the “Tomb Raider
+            temple”. Chances are you've seen pictures of this place before
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.pexels.com/photos/1089302/pexels-photo-1089302.jpeg?auto=compress&cs=tinysrgb&w=400&h=150&dpr=2"
+            alt=""
+          ></img>
+        </div>
+        <div className="content">
+          <h2>Temple from AngKor</h2>
+          <p className="info">
+            <a className="author">Aditya R</a>
+            <time>2022-04-01 15:45</time>
+          </p>
+          <p className="summary">
+            The Ta Prohm Temple (and its famous Angkor Wat tree) is one of the
+            most photogenic temples in Cambodia. It's known as the “Tomb Raider
+            temple”. Chances are you've seen pictures of this place before
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
